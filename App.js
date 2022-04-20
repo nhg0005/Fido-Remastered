a2e8f0eb67bb5c219acf9b4154bd185fc96a477a
Naeem Ghossein <nhg0005@icloud.com>
2022-04-20 15:57:30 -0500

    Moved home screen to it's own file.

--- App.js
+++ App.js
@@ -19,6 +19,7 @@ import {
 } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {HomeScreen} from './pages/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -48,84 +49,5 @@ const App = () => {
     </NavigationContainer>
   );
 };
-
-const HomeScreen = () => {
-  return (
-    <SafeAreaView
-      style={containerStyles.main}
-      contentInsetAdjustmentBehavior="automatic">
-      <StatusBar barStyle="light-content" />
-      <ScrollView style={{padding: 20}}>
-        <SectionList
-          sections={DATA}
-          keyExtractor={(item, index) => item + index}
-          renderSectionHeader={({section: {title}}) => (
-            <Text style={textStyles.sectionHeader}>{title}</Text>
-          )}
-          renderItem={({item}) => <Item title={item} />}
-        />
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-// Dummy data
-const DATA = [
-  {
-    title: 'To do',
-    data: ['Make doc appt', 'Get more dog food'],
-  },
-  {
-    title: 'Meals',
-    data: ['Lunch', 'Dinner', 'Denta-stix'],
-  },
-  {
-    title: 'Walks',
-    data: ['Water', 'Coke', 'Beer'],
-  },
-  {
-    title: 'Medication',
-    data: ['Heart worm prevention', 'Flea and tick prevention'],
-  },
-  {
-    title: 'Appointments',
-    data: ['Annual check up'],
-  },
-];
-
-const Item = ({title}) => (
-  <View style={containerStyles.listItem}>
-    <Text style={textStyles.listItem}>{title}</Text>
-  </View>
-);
-
-const containerStyles = StyleSheet.create({
-  main: {
-    flex: 1,
-    backgroundColor: '#5C6EA2',
-  },
-  listItem: {
-    backgroundColor: 'rgba(194, 199, 197, 0.5)',
-    borderRadius: 7,
-    height: 50,
-    marginVertical: 10,
-    justifyContent: 'center',
-  },
-});
-
-const textStyles = StyleSheet.create({
-  sectionHeader: {
-    fontSize: 22,
-    color: '#C2C7C5',
-    fontWeight: '600',
-    marginVertical: 5,
-  },
-  listItem: {
-    fontSize: 17,
-    marginHorizontal: 8,
-  },
-});
-
-const buttonStyles = StyleSheet.create({});
 
 export default App;
---
+++ pages/HomeScreen.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  SectionList,
+  ListItem,
+} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+/**************
+ * Page
+ **************/
+export const HomeScreen = () => {
+  return (
+    <SafeAreaView
+      style={containerStyles.main}
+      contentInsetAdjustmentBehavior="automatic">
+      <StatusBar barStyle="light-content" />
+      <ScrollView style={{padding: 20}}>
+        <SectionList
+          sections={DATA}
+          keyExtractor={(item, index) => item + index}
+          renderSectionHeader={({section: {title}}) => (
+            <Text style={textStyles.sectionHeader}>{title}</Text>
+          )}
+          renderItem={({item}) => <Item title={item} />}
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+/**************
+ * Components
+ **************/
+const Item = ({title}) => (
+  <View style={containerStyles.listItem}>
+    <Text style={textStyles.listItem}>{title}</Text>
+  </View>
+);
+
+// Dummy data
+const DATA = [
+  {
+    title: 'To do',
+    data: ['Make doc appt', 'Get more dog food'],
+  },
+  {
+    title: 'Meals',
+    data: ['Lunch', 'Dinner', 'Denta-stix'],
+  },
+  {
+    title: 'Walks',
+    data: ['Water', 'Coke', 'Beer'],
+  },
+  {
+    title: 'Medication',
+    data: ['Heart worm prevention', 'Flea and tick prevention'],
+  },
+  {
+    title: 'Appointments',
+    data: ['Annual check up'],
+  },
+];
+
+/**************
+ * Styling
+ **************/
+const containerStyles = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: '#5C6EA2',
+  },
+  listItem: {
+    backgroundColor: 'rgba(194, 199, 197, 0.5)',
+    borderRadius: 7,
+    height: 50,
+    marginVertical: 10,
+    justifyContent: 'center',
+  },
+});
+
+const textStyles = StyleSheet.create({
+  sectionHeader: {
+    fontSize: 22,
+    color: '#C2C7C5',
+    fontWeight: '600',
+    marginVertical: 5,
+  },
+  listItem: {
+    fontSize: 17,
+    marginHorizontal: 8,
+  },
+});
+
+const buttonStyles = StyleSheet.create({});
